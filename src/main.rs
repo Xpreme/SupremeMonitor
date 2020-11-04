208620eb22b6e8061e01b3e71d63428002a232f7
xvi <jrip03@gmail.com>
2020-11-03 19:59:07 -0500

    added looping | needs perf upgrades | needs comparison and send

--- src/main.rs
+++ src/main.rs
@@ -20,22 +20,23 @@ use serde::{
 };
 
 use tokio;
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Json Objects
 
 #[derive(Serialize, Deserialize)]
-struct product_ids {
-    acces: Vec<u32>, 
-    bags: Vec<u32>,
-    hats: Vec<u32>,
-    jackets: Vec<u32>,
-    pants: Vec<u32>,
-    shirts: Vec<u32>,
-    shorts: Vec<u32>,
-    skate: Vec<u32>,
-    sweatshirts: Vec<u32>,
-    tops: Vec<u32>,
-    new: Vec<u32>,
+struct ProductIds {
+    Accessories: Vec<u32>, 
+    Bags: Vec<u32>,
+    Hats: Vec<u32>,
+    Jackets: Vec<u32>,
+    Pants: Vec<u32>,
+    Shirts: Vec<u32>,
+    Shorts: Vec<u32>,
+    Skate: Vec<u32>,
+    Sweatshirts: Vec<u32>,
+    Tops: Vec<u32>,
+    New: Vec<u32>,
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -45,37 +46,39 @@ struct product_ids {
 #[tokio::main]
 async fn main() -> std::result::Result<(), Box<dyn std::error::Error + Send + Sync>> {
     
-    // building client and request (has to use tls otherwise 304 status)
-    let https = HttpsConnector::new();
-    let client = Client::builder().build::<_, hyper::Body>(https);
-    let mobile_endpoint = "https://www.supremenewyork.com/mobile_stock.json".parse()?;
+    loop {
+        // building client and request (has to use tls otherwise 304 status)
+        let https = HttpsConnector::new();
+        let client = Client::builder().build::<_, hyper::Body>(https);
+        let mobile_endpoint = "https://www.supremenewyork.com/mobile_stock.json".parse()?;
 
-    //executing request
-    //TODO: needs a loop
+        //executing request
+        //TODO: needs a loop
 
-    let resp = client
-        .get(mobile_endpoint)
-        .await?;
+        let resp = client
+            .get(mobile_endpoint)
+            .await?;
  
-    println!("status: {:#?}", resp.status());
+        println!("status: {:#?}", resp.status());
 
-    //going from stream to str
-    let bod_byte = body::to_bytes(resp.into_body()).await?;
-    let body = String::from_utf8(bod_byte.to_vec())
-        .expect("resp not utf8");
+        //going from stream to str
+        let bod_byte = body::to_bytes(resp.into_body()).await?;
+        let body = String::from_utf8(bod_byte.to_vec())
+            .expect("resp not utf8");
 
-    //so we can use this str as json
-    let v: Value = serde_json::from_str(&body)?; 
+        //so we can use this str as json
+        let v: Value = serde_json::from_str(&body)?; 
 
-    for (key, _value) in v["products_and_categories"].as_object().unwrap() {
-        println!("{}", key);
-        for (value) in v["products_and_categories"][key].as_array().unwrap(){
-            println!("{}", value["id"]);
+        for (key, _value) in v["products_and_categories"].as_object().unwrap() {
+            println!("{}", key);
+            for value in v["products_and_categories"][key].as_array().unwrap(){
+                 
+                println!("{}", value["id"]);
+            }
         }
     }
-
-    println!("body: {}", body);
-
-    Ok(())
+    //println!("body: {}", body);
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
